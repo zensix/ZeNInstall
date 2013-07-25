9f6cf71d4be06027d0c35ffe54599156597202e9
zensix <stef.zenou@gmail.com>
2013-07-25 07:08:53 +0200

    ajout modules

--- app/controllers/engine_controller.rb
+++ app/controllers/engine_controller.rb
@@ -16,15 +16,87 @@ class EngineController < ApplicationController
 	@log.message = message
 	@log.save
   end
-
+  
+  def module_header(uuid)
+    global=generateglobal
+	server = Server.find_by_uuid(uuid)
+	system = System.find(server[:system_id])
+	site=Site.find(server[:site_id])
+	sitedest=Site.find(server[:sitedestination])
+	famille=Famille.find(system[:famille_id])
+	temp="#--------------------------------------------------------------------------\n"
+	if famille[:name] == "Red Hat"
+		temp=temp+"# Script KickStart genere par ZeNinstall \n"
+	else
+		temp=temp+"# Script Preseed genere par ZeNinstall \n"
+	end
+	temp=temp+"# Hostname : #{server[:name]}\n"
+	temp=temp+"# UUID : #{server[:uuid]}\n"
+	temp=temp+"# Date :"+DateTime.current().to_s+"\n"
+	temp=temp+"# OS : #{system[:name]}\n"
+	temp=temp+"#--------------------------------------------------------------------------\n"
+	temp
+  end
+  
+  def module_postinstallos(uuid)
+    global=generateglobal
+	server = Server.find_by_uuid(uuid)
+	system = System.find(server[:system_id])
+	famille=Famille.find(system[:famille_id])
+	temp=""
+	if famille[:name] == "Red Hat"
+		logaction("info","postinstallos Red HAT")
+		temp=temp+"%post --log=/root/ks-post.log\n"
+		temp=temp+'wget --no-proxy @global[:postconfig_protocol]://@global[:postconfig_server]@global[:postconfig_call]/@server[:uuid] -O  /root/postinstall'+"\n"
+		temp=temp+'wget --no-proxy @global[:postconfig_protocol]://@global[:postconfig_server]@global[:projection_call]/@server[:uuid] -O  /root/projection.sh'+"\n"
+		temp=temp +"chmod u+x   /root/postinstall\n"
+		temp=temp +"chmod u+x   /root/projection.sh\n"
+		temp=temp +"/root/postinstall\n"
+		temp=temp + "/sbin/reboot\n"
+	elsif famille[:name] == "Debian"
+		temp=temp+'	d-i preseed/late_command string \\'+"\n"
+		temp=temp+'  umount /target/dummy;\\'+"\n"
+		temp=temp+'lvremove -f /dev/mapper/vg_sys-lv_dummy;\\'+"\n"
+		temp=temp+'   grep -v dummy /target/etc/fstab > /target/etc/fstab_save ;\\'+"\n"
+		temp=temp+'   mv /target/etc/fstab_save  /target/etc/fstab ;\\'+"\n"
+		temp=temp+'   wget @global[:postconfig_protocol]://@global[:postconfig_server]@global[:postconfig_call]/@server[:uuid]-O /target/root/postconfig;\\'+"\n"
+		temp=temp+'chmod ug+x /target/root/postconfig;\\'+"\n"
+		temp=temp+'chroot /target  bash /root/postconfig;\\'+"\n"
+		temp=temp+'echo "***preseed late finish `date`***" >> /target/var/log/postinstallscript.log\;\\'+"\n"
+		temp=temp+'reboot'
+	elsif famille[:name] == "Ubuntu"
+		temp=temp+'d-i preseed/late_command string \\' +"\n"
+		temp=temp+'in-target /bin/umount /dummy;\\' +"\n"
+		temp=temp+'in-target /bin/rmdir  /dummy;\\'+"\n"
+		temp=temp+'lvremove -f /dev/system/dummy;\\'+"\n"
+		temp=temp+'grep -v dummy /target/etc/fstab > /target/etc/fstab_tmp ;\\' +"\n"
+		temp=temp+'cat /target/etc/fstab_tmp > /target/etc/fstab ;\\' +"\n"
+		temp=temp+'in-target /usr/bin/wget  --no-proxy @global[:postconfig_protocol]://@global[:postconfig_server]@global[:postconfig_call]/@server[:uuid] -O  /root/postinstall ; \\'+"\n"
+		temp=temp+'in-target /usr/bin/wget --no-proxy  @global[:postconfig_protocol]://@global[:postconfig_server]@global[:projection_call]/@server[:uuid]-O  /root/projection.sh; \\'+"\n"
+		temp=temp+'in-target /bin/chmod u+x  /root/postinstall ; \\'+"\n"
+		temp=temp+'in-target /root/postinstall; \\' +"\n"
+		temp=temp+'reboot'
+	end
+	temp
+end
+  
   def renderscript( templatescript , server )
-    outputscript="#{templatescript}"
+    outputscriptmodule="#{templatescript}"
     system = System.find(server[:system_id])
     site=Site.find(server[:site_id])
 	sitedest=Site.find(server[:sitedestination])
     global=generateglobal
- #   boucle de remplacement
-    templatescript.scan(/(@\w+\[:\w+\])/) { |m|
+	# Remplacement des modules ( format @@module_<nom>
+	templatescript.scan(/(@@module_\w+)/) { |m|
+		 logger.info "Traitement de #{m}"
+		 modulename=m[0].to_s.sub(/@@(module_\w+)/,'\1')
+		 logger.info "Traitement du module #{modulename}"
+		 code = eval "#{modulename}('#{server.uuid}')"
+		 outputscriptmodule.sub!(m[0].to_s,"#{code}")
+	}
+ #   Remplacement des variables @domain[:valeur]
+    outputscript="#{outputscriptmodule}"
+    outputscriptmodule.scan(/(@\w+\[:\w+\])/) { |m|
       logger.info "Traitement de #{m}"
       source=m[0].to_s.sub(/@(\w+)\[:\w+\]/,'\1')
       param=m[0].to_s.sub(/@\w+\[:(\w+)\]/,'\1')
